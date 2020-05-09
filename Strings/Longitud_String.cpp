537f96e4cdd819001708989e755ad993cc7e2622
Miguel Mares <mares112358@gmail.com>
2020-05-09 17:15:55 -0500

    Calcula la longitud de un string

---
+++ Strings/Longitud_String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const char *palabra = "bienvenido";
+    int i = 0;
+
+    //ciclo for hasta alcanzar al delimitador
+    for(i = 0; palabra[i] != '\0'; i++) {
+    }
+    cout << "Longitud del string: " << i << " elementos" << endl;
+
+    return 0;
+}
