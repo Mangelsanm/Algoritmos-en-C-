25cfec3c42f93c39fb400233d8e0dbf1ec5a2a61
Miguel Mares <mares112358@gmail.com>
2020-07-21 20:21:20 -0500

    Lista circular implementada

--- Listas_ligadas/2-Lista_Circular.cpp
+++ Listas_ligadas/2-Lista_Circular.cpp
@@ -5,40 +5,40 @@ struct Nodo
 {
     int dato;
     struct Nodo *next;
-}*first = NULL;
+}*Head = NULL;
 
 void crearLista(int *elementos, int nElementos) {
-    struct Nodo *ligar;
+    struct Nodo *last;
     struct Nodo *temp;
 
-    first = (struct Nodo*)malloc(sizeof(struct Nodo));
-    first->dato = elementos[0];
-    first->next = NULL;
-    //El puntero ligar servira para hacer el enlace entre nodos.
-    ligar = first;
+    Head = (struct Nodo*)malloc(sizeof(struct Nodo));
+    Head->dato = elementos[0];
+    Head->next = Head;
+    //El puntero last servira para hacer el enlace entre nodos.
+    last = Head;
 
     for(int i = 1; i < nElementos; i++) {
         //Se crea un nodo para el elemento i-esimo del arreglo.
         temp = (struct Nodo*)malloc(sizeof(struct Nodo));
         temp->dato = elementos[i];
-        temp->next = NULL;
-        //Linkea el nodo anterior (first en la primera iteracion, temp despues de la primer iteracion)
+        temp->next = last->next;
+        //Linkea el nodo anterior (Head en la primera iteracion, temp despues de la primer iteracion)
         //con el nodo creado anteriormente.
-        ligar->next = temp;
-        ligar = temp;
+        last->next = temp;
+        last = temp;
     }
 }
 
 //Funcion para imprimir la lista ligada.
 void imprimir(struct Nodo *lista) {
-    while(lista) {
+    do {
         cout << lista->dato << " ";
         lista = lista->next;
-    }
+    }while(lista != Head);
 }
 
 int main() {
     int elementos[] = {1, 5, 6, 0};
     crearLista(&elementos[0], 4);
-    imprimir(first);
+    imprimir(Head);
 }
