9ce4b9d7264cae6e7436d1bcd16c4e62b077277b
Miguel Mares <mares112358@gmail.com>
2020-07-23 23:09:57 -0500

    Funcion para insertar un nuevo nodo en una lista doblemente ligada

--- Listas_ligadas/3-Lista_DoblementeLigada.cpp
+++ Listas_ligadas/3-Lista_DoblementeLigada.cpp
@@ -35,18 +35,49 @@ void imprimir(struct Nodo *lista) {
     }
 }
 //Funcion para saber la longitud de la lista.
-void longitud(struct Nodo *lista) {
+int longitud(struct Nodo *lista) {
     int contador = 0;
     while(lista != NULL) {
         contador++;
         lista = lista->next;
     }
-    cout << "Longitud: " << contador << endl;
+    return contador;
+}
+//Funcion para insertar un nuevo nodo a la lista.
+void insertar(struct Nodo *lista, int indice, int valor) {
+    struct Nodo *temporal;
+
+    if(indice < 0 || indice > longitud(lista)){
+        return;
+    }
+
+    if(indice == 0) {
+        temporal = (struct Nodo*)malloc(sizeof(struct Nodo));
+        temporal->dato = valor;
+        temporal->prev = NULL;
+        temporal->next = first;
+        first->prev = temporal;
+        first = temporal;
+    }
+    else {
+        for(int i = 0; i < indice - 1; i++) {
+            lista = lista->next;
+        }
+        temporal = (struct Nodo*)malloc(sizeof(struct Nodo));
+        temporal->dato = valor;
+        temporal->prev = lista;
+        temporal->next = lista->next;
+        if(lista->next) {
+            lista->next->prev = temporal;
+        }
+        lista->next = temporal;
+    }
 }
 
 int main() {
     int elementos[] = {5, 9, 3, 7, 1};
     crear(&elementos[0], 5);
-    longitud(first);
+    insertar(first, 5, 4);
+    cout << "Longitud: " << longitud(first) << endl;
     imprimir(first);
 }
