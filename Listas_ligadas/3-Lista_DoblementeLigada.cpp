025c97b3d448fbedf787c0778dc9cd3d245d6804
Miguel Mares <mares112358@gmail.com>
2020-07-23 21:25:26 -0500

    Se crearon las funciones para imprimir y saber la longitud de una lista doblemente ligada

--- Listas_ligadas/3-Lista_DoblementeLigada.cpp
+++ Listas_ligadas/3-Lista_DoblementeLigada.cpp
@@ -27,3 +27,26 @@ void crear(int *elementos, int nElementos) {
         final = temporal;
     }
 }
+//Funcion para imprimir la lista ligada.
+void imprimir(struct Nodo *lista) {
+    while(lista != NULL) {
+        cout << lista->dato << " ";
+        lista = lista->next;
+    }
+}
+//Funcion para saber la longitud de la lista.
+void longitud(struct Nodo *lista) {
+    int contador = 0;
+    while(lista != NULL) {
+        contador++;
+        lista = lista->next;
+    }
+    cout << "Longitud: " << contador << endl;
+}
+
+int main() {
+    int elementos[] = {5, 9, 3, 7, 1};
+    crear(&elementos[0], 5);
+    longitud(first);
+    imprimir(first);
+}
