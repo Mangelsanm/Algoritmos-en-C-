dc20a03915a41ae531fe857279dee2958f5c2ba3
Miguel Mares <mares112358@gmail.com>
2020-09-19 22:02:05 -0500

    Se agrego la funcion peek, para saber el valor de la posicion que se desee

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -23,6 +23,7 @@ void print(struct Stack *st) {
     for(int i = st->top; i >= 0; i--) {
         printf("%d ", st->arr[i]);
     }
+    printf("\n");
 }
 
 void push(struct Stack *st, int value) {
@@ -47,17 +48,33 @@ int pop(struct Stack *st) {
     return value;
 }
 
+int peek(struct Stack *st, int position) {
+    int x = -1;
+    if(st->top - position + 1 < 0) {
+        printf("Invalid index\n");
+    }
+    else {
+        x = st->arr[st->top - position + 1];
+    }
+    return x;
+}
+
 int main() {
     struct Stack st;
     create(&st);
 
+    /*Las siguientes lineas agregan un valor al stack*/
     push(&st, 4);
     push(&st, 1);
     push(&st, 13);
     push(&st, 7);
     push(&st, 2);
+    /*Funcion para borrar un elemento*/
     pop(&st);
+    /*Se impreme el contenido del Stack segun el valor de la variable top*/
     print(&st);
+    /*Un Stack es LIFO, peek regresara la posicion de acuerdo este acuerdo LIFO*/
+    printf("Valor de la posicion deseada: %d", peek(&st, 3));
 
     return 0;
 }
